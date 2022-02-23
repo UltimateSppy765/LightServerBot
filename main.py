dedc33dea6853e405981a9379419e1a24fa8693c
UltimateSppy765 <tapash2004@outlook.com>
2022-02-23 09:41:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import disnake as discord
+from disnake.ext import commands
+
+client=commands.Bot(command_prefix='!',intents=discord.Intents.all(),sync_commands=False,description='Let the light shine!',owner_id=770542184310243342,activity=discord.Activity(type=discord.ActivityType.watching,name='hopes rise'),allowed_mentions=discord.AllowedMentions(roles=False))
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+    print(f'Logged in as {client.user.name} - {client.user.id}')
+
+@client.command()
+async def ping(ctx):
+    """Shows the latency of the bot."""
+    await ctx.reply(f'Ping is `{round(client.latency*1000)}ms`',mention_author=False)
+
+client.run(os.environ['BOT_TOKEN'])

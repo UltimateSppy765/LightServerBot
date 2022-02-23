842d8edde895747a76c412799afec7a384d9fe26
UltimateSppy765 <tapash2004@outlook.com>
2022-02-23 14:16:54 +0530

    Change status

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from disnake.ext import commands
 
 class PersistentView(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix='!',intents=discord.Intents.all(),sync_commands=False,description='Let the light shine!',owner_id=770542184310243342,activity=discord.Activity(type=discord.ActivityType.watching,name='hopes rise'),allowed_mentions=discord.AllowedMentions(roles=False))
+        super().__init__(command_prefix='!',intents=discord.Intents.all(),sync_commands=False,description='Let the light shine!',owner_id=770542184310243342,activity=discord.Activity(type=discord.ActivityType.watching,name='hopes rise!'),allowed_mentions=discord.AllowedMentions(roles=False))
         self.persistent_views_added=False
 
     async def on_ready(self):

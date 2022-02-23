ad4faf00d8ca521a89f06c08061d9fe9102b4cc1
UltimateSppy765 <tapash2004@outlook.com>
2022-02-23 13:13:27 +0530

    Testing

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import os
 import disnake as discord
 from disnake.ext import commands
 
-client=commands.Bot(command_prefix='!',intents=discord.Intents.all(),sync_commands=False,description='Let the light shine!',owner_id=770542184310243342,activity=discord.Activity(type=discord.ActivityType.watching,name='hopes rise'),allowed_mentions=discord.AllowedMentions(roles=False))
+class PersistentView(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix='!',intents=discord.Intents.all(),sync_commands=False,description='Let the light shine!',owner_id=770542184310243342,activity=discord.Activity(type=discord.ActivityType.watching,name='hopes rise'),allowed_mentions=discord.AllowedMentions(roles=False))
+        self.persistent_views_added=False
 
-@client.event
+    async def on_ready(self):
+        print('Bot is ready.')
+        print(f'Logged in as {client.user.name} - {client.user.id}')
+
+clientt=commands.Bot(command_prefix='!',intents=discord.Intents.all(),sync_commands=False,description='Let the light shine!',owner_id=770542184310243342,activity=discord.Activity(type=discord.ActivityType.watching,name='hopes rise'),allowed_mentions=discord.AllowedMentions(roles=False))
+
+client=PersistentView()
+
+@clientt.event
 async def on_ready():
     print('Bot is ready.')
     print(f'Logged in as {client.user.name} - {client.user.id}')

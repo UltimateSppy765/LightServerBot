d456f93a0bf31e672d1b954f2c3d06f105b7deb3
UltimateSppy765 <tapash2004@outlook.com>
2022-02-23 14:29:17 +0530

    Remove extra client instance

--- main.py
+++ main.py
@@ -13,17 +13,9 @@ class PersistentView(commands.Bot):
             for i in PersistentViews.perviews():
                 self.add_view(i)
             self.persistent_views_added=True
-        print('Bot is ready.')
-        print(f'Logged in as {client.user.name} - {client.user.id}')
-
-clientt=commands.Bot(command_prefix='!',intents=discord.Intents.all(),sync_commands=False,description='Let the light shine!',owner_id=770542184310243342,activity=discord.Activity(type=discord.ActivityType.watching,name='hopes rise'),allowed_mentions=discord.AllowedMentions(roles=False))
+        print(f'Bot is ready.\nLogged in as {client.user.name} - {client.user.id}')
 
 client=PersistentView()
-
-@clientt.event
-async def on_ready():
-    print('Bot is ready.')
-    print(f'Logged in as {client.user.name} - {client.user.id}')
 
 @client.command()
 async def ping(ctx):

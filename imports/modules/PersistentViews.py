d400bd97f54622b93cd3839519bb583d1d069f31
UltimateSppy765 <tapash2004@outlook.com>
2022-02-23 13:40:22 +0530

    Update PersistentViews.py

--- imports/modules/PersistentViews.py
+++ imports/modules/PersistentViews.py
@@ -1,1 +1,13 @@
+import os
 import disnake as discord
+
+def previews():
+    return [ToggleAdmin()]
+
+class ToggleAdmin(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(custom_id=os.environ['toggle_admin_role'])
+    async def adminroletoggle(self,button,itr):
+        await itr.response.send_message('Hello World!')

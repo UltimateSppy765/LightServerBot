b8bbdec6679ae6be2697e57a9a3730902d4d1ad7
UltimateSppy765 <tapash2004@outlook.com>
2022-02-23 18:04:46 +0000

    Save stuffffff

--- imports/modules/keep_alive.py
+++ imports/modules/keep_alive.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,redirect
+import os
+from flask import Flask,render_template,redirect,request
 from threading import Thread
 
 app=Flask('')
@@ -7,12 +8,18 @@ app=Flask('')
 def main():
     return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
+@app.route('/join')
+def joinserver():
+    return redirect('https://discord.com/api/oauth2/authorize?client_id=943942461892489296&redirect_uri=https%3A%2F%2Flightserverbot.ultimatesppy765.repl.co%2Flightserver&response_type=code&scope=guilds.join%20identify%20guilds')
+
 @app.route('/alive')
 def alive():
-    return render_template('index.html')
+    return render_template('index.html',text="The bot is alive!")
 
 @app.route('/lightserver')
 def lightserverjoin():
+    codee=request.args.get('code')
+    print(codee)
     return 
 
 def run():

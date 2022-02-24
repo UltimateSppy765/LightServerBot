0ad052ce5b0d2577f8d26a129afc6b6e125a835f
UltimateSppy765 <tapash2004@outlook.com>
2022-02-24 18:07:12 +0000

    Save

--- imports/modules/keep_alive.py
+++ imports/modules/keep_alive.py
@@ -4,11 +4,28 @@ from threading import Thread
 
 app=Flask('')
 
-async def testfetchmember():
+async def testfetchmember(code:str):
     async with aiohttp.ClientSession() as session:
-         async with session.get('https://discord.com/api/v10/guilds/943965618976210965/members/770542184310243342',headers={"Authorization":f"Bot {os.environ['BOT_TOKEN']}"}) as resp:
-             a=await resp.json()
-    print(a)
+        async with session.post('https://discord.com/api/v10/oauth2/token',headers={'Content-Type':'application/x-www-form-urlencoded'},data={'grant_type':'authorization_code','code':code,'redirect_uri':'https://lightserverbot.ultimatesppy765.repl.co/lightserver'},auth=aiohttp.BasicAuth(os.environ['CLIENT_ID'],os.environ['CLIENT_SECRET'])) as resp:
+            somejson1=await resp.json()
+            atoken=somejson1['access_token']
+
+        async with session.get('https://discord.com/api/v10/users/@me',headers={'Authorization':f'Bearer {atoken}'}) as resp:
+            somejson2=await resp.json()
+            user_id=somejson2['id']
+        
+        async with session.get(f'https://discord.com/api/v10/guilds/943965618976210965/members/{user_id}',headers={"Authorization":f"Bot {os.environ['BOT_TOKEN']}"}) as resp:
+            response2=await resp.json()
+            try:
+                if response2['code']==10007:
+                    pass
+                else:
+                    return response2
+            except KeyError:
+                return "Member already in server."
+
+        async with session.put(f'https://discord.com/api/v10/guilds/943965618976210965/members/{user_id}',headers={'Authorization':f'Bearer {atoken}'},json={'roles':['944108632088387594']}) as resp:
+            return "Added to server!"
 
 @app.route('/')
 async def main():
@@ -22,15 +39,11 @@ async def joinserver():
 async def lightserverjoin():
     if request.args.get('error')=="access_denied":
         return redirect(os.environ['access_denied_auth'])
-    print(request.args.get('error'))
-    codee=request.args.get('code')
-    print(codee)
-    return 
+    return await testfetchmember(request.args.get('code'))
 
 @app.route('/test')
 async def test():
-    await testfetchmember()
-    return "Check Console?"
+    return await testfetchmember(int(request.args.get('user')))
 
 @app.errorhandler(404)
 async def page_not_found(err):

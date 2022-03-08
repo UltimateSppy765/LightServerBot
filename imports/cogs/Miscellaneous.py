f2b51aaf9a8cb5faba42da5ee882e92f26c7e220
UltimateSppy765 <tapash2004@outlook.com>
2022-03-08 18:42:28 +0530

    I stg, why

--- imports/cogs/Miscellaneous.py
+++ imports/cogs/Miscellaneous.py
@@ -18,7 +18,7 @@ class Miscellaneous(commands.Cog):
         inv=await channel.create_invite(max_age=30,max_uses=1,target_type=discord.InviteTarget.embedded_application,target_application=discord.PartyType.watch_together,reason=f'Watch Together invite requested by {str(itr.author)}')
         await itr.response.send_message(f':postbox: A one time invite has been generated to start Watch Together in {channel.mention}. This is valid for 30 seconds. Do not share this outside the server.\n{inv.url}',ephemeral=True)
 
-    @ytinv.error()
+    @ytinv.error
     async def ytinv_error(self,itr,err):
         if isinstance(err,commands.CommandOnCooldown):
             return await itr.response.send_message(f':hourglass: You are on cooldown. Please try again in {round(err.retry_after)} seconds.')

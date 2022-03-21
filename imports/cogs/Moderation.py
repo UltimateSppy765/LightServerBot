eae2bd9ea1555eef8c4c3a373e33f34bd8cb6c43
UltimateSppy765 <tapash2004@outlook.com>
2022-03-22 03:28:47 +0530

    Add `/wipe between`

--- imports/cogs/Moderation.py
+++ imports/cogs/Moderation.py
@@ -9,7 +9,7 @@ def admin_only():
     return commands.check(predicate)
 
 class WipeChecks():
-    def __init__(self,count:int,user_id:int=None,text:str=None):
+    def __init__(self,count:int=None,user_id:int=None,text:str=None):
         self.counter=1
         self.count=count
         self.user_id=user_id
@@ -37,6 +37,18 @@ class WipeChecks():
                 return True
         else:
             return False
+        
+    def bwcheck(self,m):
+        if self.textchk:
+            if self.user_id:
+                return self.textchk in m.content.lower() and self.user_id==m.author.id
+            else:
+                return self.textchk in m.content.lower()
+        else:
+            if self.user_id:
+                return self.user_id==m.author.id
+            else:
+                return True
 
 class Wipedone(discord.ui.View):
     def __init__(self,followup):
@@ -89,7 +101,7 @@ class Moderation(commands.Cog):
     @wipe.sub_command()
     async def off(self,itr,count:int=20,hidden:bool=False):
         await itr.response.defer(ephemeral=hidden)
-        pur=await itr.channel.purge(limit=count,before=itr,after=discord.utils.snowflake_time(itr.id)-timedelta(days=14),bulk=True,oldest_first=False)
+        pur=await itr.channel.purge(limit=count,before=itr,after=itr.created_at-timedelta(days=14),bulk=True,oldest_first=False)
         view=Wipedone(followup=itr.followup) if not hidden else None
         await itr.edit_original_message(content=f":broom: Successfully wiped {len(pur)} message{'s' if len(pur)>1 else ''}." if len(pur)>0 else ":negative_squared_cross_mark: No messages were wiped.",view=view)
         if not hidden:
@@ -98,7 +110,7 @@ class Moderation(commands.Cog):
     @wipe.sub_command()
     async def user(self,itr,user:discord.User,count:int=20,hidden:bool=False):
         await itr.response.defer(ephemeral=hidden)
-        pur=await itr.channel.purge(check=WipeChecks(count=count,user_id=user.id).usercheck,limit=1000,before=itr,after=discord.utils.snowflake_time(itr.id)-timedelta(days=14),bulk=True,oldest_first=False)
+        pur=await itr.channel.purge(check=WipeChecks(count=count,user_id=user.id).usercheck,limit=500,before=itr,after=itr.created_at-timedelta(days=14),bulk=True,oldest_first=False)
         view=Wipedone(followup=itr.followup) if not hidden else None
         await itr.edit_original_message(content=f":broom: Successfully wiped {len(pur)} message{'s' if len(pur)>1 else ''}." if len(pur)>0 else ":negative_squared_cross_mark: No messages were wiped.",view=view)
         if not hidden:
@@ -107,7 +119,18 @@ class Moderation(commands.Cog):
     @wipe.sub_command()
     async def text(self,itr,text:str,user:discord.User=None,count:int=20,hidden:bool=False):
         await itr.response.defer(ephemeral=hidden)
-        pur=await itr.channel.purge(check=WipeChecks(count=count,text=text.strip().lower(),user_id=user.id if user else None).hastextcheck,limit=1000,before=itr,after=discord.utils.snowflake_time(itr.id)-timedelta(days=14),bulk=True,oldest_first=False)
+        pur=await itr.channel.purge(check=WipeChecks(count=count,text=text.strip().lower(),user_id=user.id if user else None).hastextcheck,limit=500,before=itr,after=itr.created_at-timedelta(days=14),bulk=True,oldest_first=False)
+        view=Wipedone(followup=itr.followup) if not hidden else None
+        await itr.edit_original_message(content=f":broom: Successfully wiped {len(pur)} message{'s' if len(pur)>1 else ''}." if len(pur)>0 else ":negative_squared_cross_mark: No messages were wiped.",view=view)
+        if not hidden:
+            view.message=await itr.original_message()
+            
+    @wipe.sub_command()
+    async def between(self,itr,before:discord.Object,after:discord.Object,user:discord.User=None,match:str=None,hidden:bool=False):
+        if before.created_at>after.created_at:
+            return await itr.response.send_message(':x: The `before` parameter cannot contain a message/timeframe after the one in the `after` parameter.',ephemeral=True)
+        await itr.response.defer(ephemeral=hidden)
+        pur=await itr.channel.purge(limit=500,bulk=True,oldest_first=False,before=before.created_at if before.created_at<itr.created_at else itr.created_at,after=after.created_at if after.created_at>itr.created_at-timedelta(days=14) else itr.created_at-timedelta(days=14),check=check=WipeChecks(text=match.strip().lower() if match else None,user_id=user.id if user else None).bwcheck)
         view=Wipedone(followup=itr.followup) if not hidden else None
         await itr.edit_original_message(content=f":broom: Successfully wiped {len(pur)} message{'s' if len(pur)>1 else ''}." if len(pur)>0 else ":negative_squared_cross_mark: No messages were wiped.",view=view)
         if not hidden:
